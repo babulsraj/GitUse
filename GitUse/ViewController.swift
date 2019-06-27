73eb2a157bba5f36c0d054af5e82cd8e6d2b3bf2
BABUL S RAJ <babulsraj@gmail.com>
2019-06-27 18:04:08 +0530

    push push

--- GitUse/ViewController.swift
+++ GitUse/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("second commit")
         print("third commit")
+        print("after the error")
         // Do any additional setup after loading the view.
     }
     
